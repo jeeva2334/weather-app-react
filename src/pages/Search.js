53c1705de98c810b5558888d67d1fa034520f159
jeeva <101379886+jeeva2334@users.noreply.github.com>
2023-11-22 16:03:40 +0530

    Update Search.js

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -27,7 +27,7 @@ function Search() {
                 <div className="absolute top-4 left-3">
                     <i className="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
                 </div>
-                <input type="text" className="h-14 w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none" placeholder="City name here" name="city" onChange={e=>setCity(e.target.value)}/>
+                <input type="text" className="h-14 w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none" placeholder="Enter an city name" name="city" onChange={e=>setCity(e.target.value)}/>
                 <div className="absolute top-2 right-2">
                     <button className="h-10 w-20 text-white rounded-lg bg-[#2e3239] hover:bg-[#282b30]" onClick={()=>search()}>Search</button>
                 </div>
